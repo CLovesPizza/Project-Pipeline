e79f3c99304321dd059878e45808d52083ec53ce
Caleb Hanneman <calebhanneman@new.rr.com>
2016-04-23 22:05:24 -0500

    Cleanup!
    Added support for additional LEDs!

--- Test_Code.ino
+++ Test_Code.ino
@@ -1,4 +1,3 @@
-byte LED = 13;
 byte sensorInterrupt = 0;
 byte sensorPin       = 2;
 float calibrationFactor = 4.5;
@@ -9,42 +8,62 @@ unsigned long totalMilliLitres;
 unsigned long oldTime;
 unsigned long Gallons;
 long conversionFactor = 0.000264172;
+
 void setup()
 {
-  pinMode(LED, OUTPUT);
-  digitalWrite(LED, HIGH);
+  pinMode(8, OUTPUT); //Green
+  pinMode(9, OUTPUT); //Blue
+  pinMode(10, OUTPUT); //Red
   
-  pinMode(sensorPin, INPUT);
-  digitalWrite(sensorPin, HIGH);
+    pinMode(sensorPin, INPUT);
+    digitalWrite(sensorPin, HIGH);
 
-  pulseCount        = 0;
-  flowRate          = 0.0;
-  flowMilliLitres   = 0;
-  totalMilliLitres  = 0;
-  oldTime           = 0;
-  attachInterrupt(sensorInterrupt, pulseCounter, FALLING);
+    pulseCount        = 0;
+    flowRate          = 0.0;
+    flowMilliLitres   = 0;
+    totalMilliLitres  = 0;
+    oldTime           = 0;
+    attachInterrupt(sensorInterrupt, pulseCounter, FALLING);
 }
+
 void loop()
 {
-   
-   if((millis() - oldTime) > 1000)  
-  { 
-    detachInterrupt(sensorInterrupt);
-    flowRate = ((1000.0 / (millis() - oldTime)) * pulseCount) / calibrationFactor;
-    oldTime = millis();
-    flowMilliLitres = (flowRate / 60) * 1000;
-    totalMilliLitres += flowMilliLitres;
-    unsigned int frac;
-    frac = (flowRate - int(flowRate)) * 10;
-    attachInterrupt(sensorInterrupt, pulseCounter, FALLING);
-  }
-
+  
   Gallons = totalMilliLitres * conversionFactor;
-
-  if(Gallons > 1)
-  {
+   
+     if((millis() - oldTime) > 1000)  
+    { 
+      detachInterrupt(sensorInterrupt);
+      flowRate = ((1000.0 / (millis() - oldTime)) * pulseCount) / calibrationFactor;
+      oldTime = millis();
+      flowMilliLitres = (flowRate / 60) * 1000;
+      totalMilliLitres += flowMilliLitres;
+      unsigned int frac;
+      frac = (flowRate - int(flowRate)) * 10;
+      attachInterrupt(sensorInterrupt, pulseCounter, FALLING);
+    }
     
-   }
+    if(Gallons > 1)
+    {
+      digitalWrite(8, HIGH);
+      digitalWrite(9, LOW);
+      digitalWrite(10, LOW);
+    }
+    
+    if(Gallons > 2)
+    {
+      digitalWrite(9, HIGH);
+      digitalWrite(8, LOW);
+      digitalWrite(10, LOW);
+    }
+    
+    if(Gallons > 3)
+    {
+      digitalWrite(10, HIGH);
+      digitalWrite(9, LOW);
+      digitalWrite(8, LOW);
+    }
+
 }
 void pulseCounter()
 {
